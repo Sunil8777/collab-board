433898b6c624dbd84f61c305feaab2e471c810b7
Sunil8777 <suniljoshi8777@gmail.com>
2025-04-06 14:19:06 +0530

    convex-client-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ConvexClientProvider>
+      <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
+    </ConvexClientProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://true-mudfish-27.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
