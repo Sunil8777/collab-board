66c2c214f62010fe199a0c2d32ee1e293863d5c9
Sunil8777 <suniljoshi8777@gmail.com>
2025-04-14 15:35:46 +0530

    schema and board card

---
+++ hooks/use-api-mutation.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "convex/react";
+import { useState } from "react";
+
+export const useApiMutation = (mutationFn:any) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const apiMutation = useMutation(mutationFn);
+
+  const mutate = async (payload:any) => {
+    setIsLoading(true)
+    try {
+        await apiMutation(payload)
+    } catch (error) {
+        throw new Error("Something went wrong")
+    }
+    finally{
+        setIsLoading(false)
+    }
+  };
+
+  return {
+    mutate,
+    isLoading,
+  };
+};
